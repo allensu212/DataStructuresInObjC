e1a475404850bddfa907a6496fce2f4e65a5be96
Wei-Lun Su <wsu8@illinois.edu>
2015-08-16 14:53:49 -0400

    StringManipulator

---
+++ DataStructuresInObjC/StringManipulator.h
@@ -0,0 +1,13 @@
+//
+//  StringManipulator.h
+//  DataStructuresInObjC
+//
+//  Created by Wei-Lun Su on 8/16/15.
+//  Copyright (c) 2015 allensu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StringManipulator : NSObject
+
+@end
---
+++ DataStructuresInObjC/StringManipulator.m
@@ -0,0 +1,13 @@
+//
+//  StringManipulator.m
+//  DataStructuresInObjC
+//
+//  Created by Wei-Lun Su on 8/16/15.
+//  Copyright (c) 2015 allensu. All rights reserved.
+//
+
+#import "StringManipulator.h"
+
+@implementation StringManipulator
+
+@end
