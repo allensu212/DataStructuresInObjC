4febdaa9d7a5b51fe0e3849837083525ab5d1910
Wei-Lun Su <wsu8@illinois.edu>
2015-08-16 21:51:15 -0400

    count number of items in queue, implement dequeue, count the peek of the queue, check whether queue is empty

--- DataStructuresInObjC/Queue.h
+++ DataStructuresInObjC/Queue.h
@@ -9,6 +9,11 @@
 #import <Foundation/Foundation.h>
 
 @interface Queue : NSObject
+
 -(void)enQueueWithKey:(NSNumber *)key;
 -(NSNumber *)deQueue;
+-(BOOL)isEmpty;
+-(int)countQueue;
+-(NSNumber *)peek;
+
 @end
